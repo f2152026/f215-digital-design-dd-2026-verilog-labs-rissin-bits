bc79cd713320d7188ff53fbda64f2092a32efc58
Rishit <f20251295@pilani.bits-pilani.ac.in>
2026-09-15 14:26:50 +0000

    lab02 task1

---
+++ labs/lab02/task1/tb.v
@@ -0,0 +1,47 @@
+// tb.v
+// Starter testbench template -- YOU complete this file.
+//
+// Goal: apply all 8 combinations of I0, I1, S (5 time units apart) to DUT
+// and observe the output. Fill in every TODO below.
+
+module tb;
+
+  reg t_i0, t_i1, t_s;
+  wire t_y;
+
+  DUT U1 (
+    .I0(t_i0),
+    .I1(t_i1),
+    .S(t_s),
+    .Y(t_y)
+  );
+
+  // Waveform dump configuration
+
+  string vcd_file;
+
+  initial begin
+    if ($value$plusargs("vcd=%s", vcd_file)) begin
+      $dumpfile(vcd_file);
+      $dumpvars(0, U1);
+    end
+  end
+
+  initial begin
+    t_i0 = 0; t_i1 = 0; t_s = 0; #5;
+    t_i0 = 0; t_i1 = 0; t_s = 1; #5;
+    t_i0 = 0; t_i1 = 1; t_s = 0; #5;
+    t_i0 = 0; t_i1 = 1; t_s = 1; #5;
+    t_i0 = 1; t_i1 = 0; t_s = 0; #5;
+    t_i0 = 1; t_i1 = 0; t_s = 1; #5;
+    t_i0 = 1; t_i1 = 1; t_s = 0; #5;
+    t_i0 = 1; t_i1 = 1; t_s = 1; #5;
+
+    $finish;
+  end
+
+  initial
+    $monitor($time, " I0=%b I1=%b S=%b | Y=%b",
+             t_i0, t_i1, t_s, t_y);
+
+endmodule
